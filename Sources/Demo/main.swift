14e0cd9a695c0c498527974b95182a77080bd7c4
Daniel <daniel@danramteke.com>
2019-08-21 06:16:14 -0400

    demo clean up

--- Sources/Demo/main.swift
+++ Sources/Demo/main.swift
@@ -1,19 +1,9 @@
 import SwiftyBuildLib
 
 
-let exportOptions = ExportOptions(method: .appstore, teamID: "PC4GHHF42K")
-// try! exportOptions.write(to: "./.swiftybuild/exportoptions.plist")
-
+let exportOptions = ExportOptions(method: .appstore, teamID: "XXXXXXXXXX")
 let options = TargetOptions(scheme: "MyScheme", workspace: "MyWorkspace")
 
-// let actions: [Action] = [
-//   WriteExportOptions(path: Path("./.swiftybuild/\(options.scheme!)-exportOptions.plist"), exportOptions: exportOptions),
-//   BuildAction(targetOptions: options, destination: .generic_iOS),
-//   BuildAction(targetOptions: options, destination: .generic_iOS),
-//   ArchiveAction(targetOptions: options, sdk: .iphoneos, archivePath: "./.swiftybuild/\(options.scheme!).xcarchive"),
-// ]
-
-// try run("MyScheme", actions)
 do {
 let context = try Context()
 try context.writeExportOptions(path: Path("./.swiftybuild/\(options.scheme!)-exportOptions.plist"), exportOptions: exportOptions)
