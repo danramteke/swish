8ad0e60764454d15e5df812ac837b5084ff07339
Daniel <daniel@danramteke.com>
2019-08-25 00:15:49 -0400

    organizing more files

--- Sources/Demo/main.swift
+++ Sources/Demo/main.swift
@@ -3,12 +3,10 @@ import SwiftyBuildLib
 let exportOptions = ExportOptions(method: .appstore, teamID: "XXXXXXXXXX")
 let options = TargetOptions(scheme: "MyScheme", workspace: "MyWorkspace")
 
-
-
 do {
   try exportOptions.write(to: "./.swiftybuild/Demo-exportOptions.plist")
   try Xcodebuild.Build(targetOptions: options, destination: .generic_iOS).run()
   try Xcodebuild.Test(targetOptions: options, destination: .generic_iOS).run()
 } catch {
-  print("Error:", error.localizedDescription)
+  print("Build Error:", error.localizedDescription)
 }
--- Sources/SwiftyBuildLib/Action-run.swift
+++ Sources/SwiftyBuildLib/Action-run.swift
@@ -32,20 +32,3 @@ extension ShellAction {
   }
 }
 
-extension Context {
-  private static var _default: Context? 
-  public static var `default`: Context {
-    if let _default = _default {
-      return _default
-    } else {
-      do {
-        let newContext = try Context(name: nil, output:"./.swiftybuild", dryRun: false)
-        Context._default = newContext
-        return newContext
-      } catch {
-        print("could not instantiate default Context.", error)
-        exit(1)
-      }
-    }
-  }
-}
