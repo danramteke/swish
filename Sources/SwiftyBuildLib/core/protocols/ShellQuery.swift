8898923e35298132a8ae6d6fdd9b584925ce91aa
Daniel R <daniel@macbookpro.local>
2021-01-15 23:12:26 -0500

    cleanup

--- Sources/SwiftyBuildLib/core/protocols/ShellQuery.swift
+++ Sources/SwiftyBuildLib/core/protocols/ShellQuery.swift
@@ -3,7 +3,7 @@ import Foundation
 public protocol ShellQuery: ShellAction {
 
   associatedtype ResultSuccessType
-  func parseResult(output: String?, error: String?) -> Result<ResultSuccessType, Error>
+  func parseResult(output: String, error: String) -> Result<ResultSuccessType, Error>
 }
 
 
@@ -12,19 +12,15 @@ extension ShellQuery {
     let logPaths = context.logPaths(for: self)
     let stdoutContents = try String(path: logPaths.stdout)
     let stderrContents = try String(path: logPaths.stderr)
-    return self.parseResult(output: stdoutContents, error: stderrContents)
+    return self.parseResult(output: stdoutContents.trimmingCharacters(in: .whitespacesAndNewlines), error: stderrContents)
   }
 }
 
 
 public extension ShellQuery where ResultSuccessType: ShellOutputInitable {
-  func parseResult(output: String?, error: String?) -> Result<ResultSuccessType, Error> {
+  func parseResult(output: String, error: String) -> Result<ResultSuccessType, Error> {
     return Result {
-      guard let output = output else {
-        throw RunError.queryHadNoOutput
-      }
-      let trimmedOutput = output.trimmingCharacters(in: .whitespacesAndNewlines)
-      return try ResultSuccessType.init(shellQueryOutput: trimmedOutput)
+      return try ResultSuccessType.init(shellQueryOutput: output)
     }
   }
 }
