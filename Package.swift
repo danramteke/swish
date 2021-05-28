cec999462f58665b7715898eb6d8a59c9d23e28c
Daniel R <danramteke>
2021-05-27 23:56:26 -0400

    clean up imports

--- Package.swift
+++ Package.swift
@@ -14,8 +14,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.0.0"),
-        .package(url: "https://github.com/mxcl/Version.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0"),
         .package(url: "https://github.com/danramteke/MPath.git", from: "0.9.7"),
     ],
     targets: [
@@ -24,22 +22,14 @@ let package = Package(
             dependencies: [
                 "SwishKit",
                 "MPath",
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]),
         .target(
             name: "SwishKit",
             dependencies: [
-              "Rainbow", "Version", "MPath",
+              "Rainbow", "MPath",
             ]),
         .testTarget(
             name: "SwishKitTests",
             dependencies: ["SwishKit"]),
     ]
 )
-
-#if !canImport(Combine)
-package.dependencies.append(.package(url: "https://github.com/OpenCombine/OpenCombine.git", from: "0.11.0"))
-package.targets.first(where: { $0.name == "SwishKit"})?.dependencies.append("OpenCombine")
-#else
-
-#endif
