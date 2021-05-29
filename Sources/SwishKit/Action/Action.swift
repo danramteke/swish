2d5965da42e1378a2d41e026e60c9585e2155439
Daniel R <danramteke>
2021-05-29 00:03:42 -0400

    move resolve to action id

--- Sources/SwishKit/Action/Action.swift
+++ Sources/SwishKit/Action/Action.swift
@@ -6,22 +6,9 @@ public protocol Action {
 	var isNeeded: Bool { get }
 
 	func execute() throws
-
-	func resolve(force: Bool) throws
 }
 
 public extension Action {
 	var dependsOn: [ActionID] { [] }
 	var isNeeded: Bool { true }
-	func resolve(force: Bool) throws {
-		try self.dependsOn.forEach {
-			try $0.action.resolve(force: force)
-		}
-
-		if self.isNeeded || force {
-			try self.execute()
-		} else {
-			//print("Skipping \(self.id), not needed".red)
-		}
-	}
 }
--- Sources/SwishKit/Action/ActionID.swift
+++ Sources/SwishKit/Action/ActionID.swift
@@ -3,10 +3,29 @@ import Foundation
 public protocol ActionID {
 	var action: Action { get }
 	var name: String { get }
+
+	func resolve(force: Bool) throws
+}
+
+public extension ActionID {
+	func resolve(force: Bool) throws {
+		let action = self.action
+		try action.dependsOn.forEach {
+			try $0.resolve(force: force)
+		}
+
+		if action.isNeeded || force {
+			try action.execute()
+		} else {
+			//print("Skipping \(self.id), not needed".red)
+		}
+	}
 }
 
 public extension ActionID where Self: RawRepresentable, RawValue == String {
-    var name: String {
-        rawValue
-    }
+	var name: String {
+		String(describing: Self.self) + rawValue
+	}
+
+
 }
