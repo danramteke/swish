a47101b1498147d0a3bdea58f239d174ae813ef6
Daniel R <danramteke>
2021-06-11 02:03:11 -0400

    target is identifiable

--- Sources/SwishKit/Shell/ShellRunner/ShellRunner.resolve.swift
+++ Sources/SwishKit/Shell/ShellRunner/ShellRunner.resolve.swift
@@ -4,12 +4,12 @@ extension ShellRunner {
 
 	public func resolve<T: TargetID>(_ targetID: T, force: Bool = false) throws {
 
-		if resolutionLog.contains(targetID.name) {
-			self.logger.alreadyResolved(targetID.name)
+		if resolutionLog.contains(targetID.id) {
+			self.logger.alreadyResolved(targetID.id)
 			return
 		}
 
-		self.logger.startResolution(of: targetID.name, dependsOn: targetID.dependsOn.map { $0.name })
+		self.logger.startResolution(of: targetID.id, dependsOn: targetID.dependsOn.map { $0.id })
 
 		for dependentTargetId in targetID.dependsOn {
 			try self.resolve(dependentTargetId, force: force)
@@ -18,9 +18,9 @@ extension ShellRunner {
 		let target = targetID.target
 		if target.isNeeded || force {
 			try target.execute()
-			self.resolutionLog.append(targetID.name)
+			self.resolutionLog.append(targetID.id)
 		} else {
-			self.logger.skipping(targetID.name)
+			self.logger.skipping(targetID.id)
 		}
 	}
 }
--- Sources/SwishKit/Target/TargetID.swift
+++ Sources/SwishKit/Target/TargetID.swift
@@ -1,17 +1,16 @@
 import Foundation
 import Logging
 
-public protocol TargetID {
-	var name: String { get }
+public protocol TargetID: Identifiable {
+	var id: String { get }
 	var target: Target { get }
 
 	var dependsOn: [Self] { get }
 }
 
-
 public extension TargetID where Self: RawRepresentable, RawValue == String {
 
-	var name: String {
+	var id: String {
 		String(describing: Self.self) + "." + rawValue
 	}
 }
