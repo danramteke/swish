ff95438c5fd036a1df643471ffb278aa22a3709d
Daniel R <danramteke>
2021-06-14 11:35:17 -0400

    print out command that was run

--- Sources/ScriptsKit/Script.execute.swift
+++ Sources/ScriptsKit/Script.execute.swift
@@ -2,25 +2,28 @@ import ScriptsDescription
 import Foundation
 
 extension Script {
-	func execute() throws {
-		switch self {
-		case .text(let text):
-			let process = Process()
-			process.executableURL = URL(fileURLWithPath: "/bin/sh")
-			process.arguments = ["-c", text]
+    func execute() throws {
+        switch self {
+        case .text(let text):
+            print("$ \(text)")
+            print()
+
+            let process = Process()
+            process.executableURL = URL(fileURLWithPath: "/bin/sh")
+            process.arguments = ["-c", text]
 
             process.standardOutput = FileHandle.standardOutput
             process.standardError = FileHandle.standardError
-			try process.run()
-			process.waitUntilExit()
+            try process.run()
+            process.waitUntilExit()
 
-			if 0 != process.terminationStatus {
-				throw NonZeroShellTermination(status: process.terminationStatus)
-			}
-		}
-	}
+            if 0 != process.terminationStatus {
+                throw NonZeroShellTermination(status: process.terminationStatus)
+            }
+        }
+    }
 }
 
 public struct NonZeroShellTermination: Error {
-	public let status: Int32
+    public let status: Int32
 }
